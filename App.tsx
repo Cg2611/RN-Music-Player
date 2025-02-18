dfb6bba5a5beac843719fc3b815b9b6d2faee8b2
Chaithanya Ganesh <chaithanyaprojects1309@gmail.com>
2025-02-18 12:07:38 +0530

    minor changes

--- App.tsx
+++ App.tsx
@@ -77,18 +77,14 @@ function App(): React.JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
+            Edit <Text style={styles.highlight}>App.tsx</Text> Testing merge request this should pop up as a conflict    </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
           </Section>
           <Section title="Debug">
             <DebugInstructions />
           </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
+        
           <LearnMoreLinks />
         </View>
       </ScrollView>
